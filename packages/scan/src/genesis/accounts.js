84b5b87738645355188fb9f4739f33c0b36b6302
chaojun <chaojun@opensquare.network>
2020-12-18 15:49:03 +0800

    Add councilor account to genesis initi

--- packages/scan/src/genesis/accounts.js
+++ packages/scan/src/genesis/accounts.js
@@ -3,6 +3,7 @@ const accounts = [
   '2fuA7fKVPGRU8DxRoX8iZxwujAYAUkAGjaXxT13Qm7ADeHMS',
   '2epdvhycxhnnoZFRB5mrFSgRAeUMM7WwyN9n9Fksh4tQpWum',
   '2hM7S93kBmuBTgLY59xL1eYPjJvzK5ojJVSYKEza5c2C8A5P',
+  '2ck7Ffrom1wmewMWUuQQYcw5tgTxiLj3c8foQ1hgc5dL2N3P',
 ]
 
 module.exports = {
